d7226dbf66e1b83973997fbde7e7164b984b253e
cakeforcat <cakeforcat@gmail.com>
2025-07-21 20:57:50 +0100

    throwing some wild guesses from different legions

--- lenovo/legion/15arh05h/default.nix
+++ lenovo/legion/15arh05h/default.nix
@@ -3,18 +3,33 @@
 {
   imports = [
     ../../../common/cpu/amd
-    ../../../common/gpu/amd
     ../../../common/gpu/nvidia/prime.nix
     ../../../common/gpu/nvidia/turing
     ../../../common/pc/laptop
     ../../../common/pc/ssd
   ];
 
-  # Specify bus id of Nvidia and Intel graphics.
-  hardware.nvidia.prime = {
-    amdgpuBusId = "PCI:6:0:0";
-    nvidiaBusId = "PCI:1:0:0";
+  # Specify bus id of Nvidia and AMD graphics.
+  hardware.nvidia = { 
+    prime = {
+      amdgpuBusId = "PCI:6:0:0";
+      nvidiaBusId = "PCI:1:0:0";
+    };
+    powerManagement.enable = lib.mkDefault true;
   };
+
+  # legion and nvidia kernel module
+  boot= { 
+    extraModulePackages = with config.boot.kernelPackages; [
+      lenovo-legion-module
+      nvidia_x11
+      ];
+    kernelModules = [ "amdgpu" ];
+  };
+  
+  services.xserver.videoDrivers = [
+    "amdgpu"
+  ];
 
   # Cooling management
   services.thermald.enable = lib.mkDefault true;
