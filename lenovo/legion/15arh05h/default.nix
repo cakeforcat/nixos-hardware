17ad518c2bcf64b0b8520087444f3dce4e36dc99
cakeforcat <cakeforcat@gmail.com>
2025-07-21 21:42:57 +0100

    amdgpu really messing things up

--- lenovo/legion/15arh05h/default.nix
+++ lenovo/legion/15arh05h/default.nix
@@ -5,7 +5,6 @@
 }: {
   imports = [
     ../../../common/cpu/amd
-    ../../../common/gpu/amd
     ../../../common/gpu/nvidia/prime.nix
     ../../../common/gpu/nvidia/turing
     ../../../common/pc/laptop
@@ -18,20 +17,14 @@
       amdgpuBusId = "PCI:6:0:0";
       nvidiaBusId = "PCI:1:0:0";
     };
-    powerManagement.enable = lib.mkDefault true;
   };
 
   # legion and nvidia kernel module
   boot = {
     extraModulePackages = with config.boot.kernelPackages; [
       lenovo-legion-module
-      nvidia_x11
     ];
   };
-
-  services.xserver.videoDrivers = [
-    "amdgpu"
-  ];
 
   # Cooling management
   services.thermald.enable = lib.mkDefault true;
